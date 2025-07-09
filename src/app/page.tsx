bf61eceb7b6aef698a0965642a6ae6850e1f2e81
Mikeps2 <mikeps.dev@gmail.com>
2025-07-09 12:43:37 -0300

    feat: estilização tela home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,29 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import styles from '../styles/page.module.css';
 import React from 'react';
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        User Registration Reference System
-      </h1>
-      <p className="mt-4">
-        This is a simple application to demonstrate user registration with
-        referral codes.
-      </p>
-      <br />
-      <button onClick={() => router.push('/login')}>
-        || Tela Login |
-      </button>
-      <button onClick={() => router.push('/account')}>
-        | Tela Account |
-      </button>
-      <button onClick={() => router.push('/onboard')}>
-        | Tela Onboard ||
-      </button>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+          Tela de Início
+        </h1>
+        <br />
+        <button onClick={() => router.push('/login')} className={styles.button}>
+          Login
+        </button>
+        <button onClick={() => router.push('/account')} className={styles.button}>
+          Account
+        </button>
+        <button onClick={() => router.push('/onboard')} className={styles.button}>
+          Onboard
+        </button>
+      </div>
     </div>
   );
 }
